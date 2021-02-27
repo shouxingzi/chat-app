ad7fbdb874c48fa788659a55d2d98ca25daa3ee2
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-02-27 21:29:45 +0900

    バリテーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  # nameカラムに送るデータが空の場合はDBに保存しない
 end
