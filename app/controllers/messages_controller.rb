e477b4ad8ca45b899a8064b8b11568986215b896
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-03-02 14:02:32 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,13 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
   # @messageにはMessage.newで生成したMessageモデルのインスタンス情報を代入している
   # @roomにはparamsに含まれているroom_idを代入している。
+  # チャットルームに紐付いている全てのメッセージ(@room.messages)を@messageと定義。一覧画面で表示するメッセージ情報にはユーザー情報も紐付いて表示される
+  # この場合メッセージに紐づくユーザー情報の取得に、メッセージの数と同じ回数のアクセスが必要になるが、includesメソッドでユーザー情報を一度のアクセスでまとめて取得している。
+
 
   def create
     @room = Room.find(params[:room_id])
@@ -12,6 +16,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :indes
     end
   end
