89e4b17f84caa25b9633c1419c77c303792847a9
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-03-03 21:32:20 +0900

    画像のみでもメッセージ送信可能となるバリテーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,10 +4,18 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
+  # has_one_attached :ファイル名
+  # 各レコードとファイルを１対１の関係で紐付けるメソッド。has_one_attachedメソッドを記述したモデルの各レコードはそれぞれ１つのファイルを添付できる
+  # この記述により、モデル.ファイル名 で、添付されたファイアにアクセスできるようになる。またファイル名はそのモデルが紐付いたフォーム空送られるパラメーターのキーにもなる
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+  # unless メソッド名   メソッドの帰り値がfalseならバリテーションによる検証を行うことを条件としている
+  # 画像が存在しなければテキストが必要となり画像があればテキストは不要になる
+
+def was_attached?
+  self.image.attached?
+  # 画像があればtrueなければfalseを返す仕組みになっている
 end
 
-# has_one_attached :ファイル名
-# 各レコードとファイルを１対１の関係で紐付けるメソッド。has_one_attachedメソッドを記述したモデルの各レコードはそれぞれ１つのファイルを添付できる
-# この記述により、モデル.ファイル名 で、添付されたファイアにアクセスできるようになる。またファイル名はそのモデルが紐付いたフォーム空送られるパラメーターのキーにもなる
+end
+
